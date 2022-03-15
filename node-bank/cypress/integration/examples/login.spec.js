5004924ede689c221a1b59c2931dd13e0d8f5d94
Jordan Lewis <56411900+JKL184@users.noreply.github.com>
2022-03-14 22:38:08 -0500

    Add files via upload

---
+++ node-bank/cypress/integration/examples/login.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/login')
+  })
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it('.type() - type into a DOM element', () => {
+    // https://on.cypress.io/type
+    cy.get('#email')
+      .type('you@example.com')
+      .should('have.value', 'you@example.com')
+
+    cy.get('#password')
+      .type('Password')
+      .should('have.value', 'Password')
+
+      
+    cy.get('#login').click()
+
+    cy.title().should('include', 'member')
+  })
+})
