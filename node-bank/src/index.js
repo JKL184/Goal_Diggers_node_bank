2047670e808075ac600a9eb490a87a2d8d826262
JKL184 <jordankareemlewis@gmail.com>
2022-03-15 19:07:47 -0500

    added isWithdrawValid and some tests

--- node-bank/src/index.js
+++ node-bank/src/index.js
@@ -11,26 +11,71 @@ var isEmailValid = function(emailAddr) {
 
 var verifyPassword = function(pw) {
 	var response = {
-		isValid : false,
+		isValid : true,
 		message : ""
 	};
 	//check empty password field  
 	if(pw == "") {  
+		response.isValid = false;
 		response.message = "Password is empty";  
 	}  
 
 	//minimum password length validation  
-	if(pw.length < 8) {  
+	if(pw.length < 8) {
+		response.isValid = false;  
 		response.message = "Password length must be atleast 8 characters";
 	}  
 	else{
 		//maximum length of password validation  
 		if(pw.length > 15) {  
+			response.isValid = false;
 			response.message = "Password length must not exceed 15 characters";  
 		}
 	}
 
-	response.isValid = true;
+	
+
+	return response;
+};
+
+var isWithdrawValid = function(withdraw,AvBal) {
+	var response = {
+		isValid : true,
+		message : ""
+	};
+	if ((/^[+]?([0-9]+(?:[\.][0-9]{0,2})?|\.[0-9]{1,2})$/.test(withdraw))) {
+		response.isValid = true;  
+	} 
+	//check empty password field  
+	if(withdraw == "") {  
+		response.isValid = false;
+		response.message = "Withdraw is empty";  
+	}  
+    //check decimal spaces 
+	if((/^[+]?([0-9]+(?:[\.][0-9][0-9][0-9]+))$/.test(withdraw))){  
+		response.isValid = false;
+		response.message = "Withdraw Amount value cannot have more than 2 decimal spaces";  
+	
+	}
+	//check for characters
+	if((/[a-zA-Z!#$%&'*+/=?^_`{|}~-]/.test(withdraw))){  
+		response.isValid = false;
+		response.message = "Withdraw Amount value cannot have letters or special characters";  
+	}   
+	//minimum withdraw value validation  
+	if(parseFloat(withdraw)<0) {  
+		response.isValid = false;
+		response.message = "Withdraw Amount value must not be below 0";  
+	}
+	else{
+		//maximum value of withdraw validation  
+		if(parseFloat(withdraw) > parseFloat(AvBal)) {  
+			response.isValid = false;
+			response.message = "Withdraw Amount value must not exceed available balance";  
+		}
+	}
+
+	
 
 	return response;
 };
--- node-bank/test/unit/index.spec.js
+++ node-bank/test/unit/index.spec.js
@@ -16,28 +16,67 @@ describe('isEmailValid', function() {
 
   it('should return false when the email is abc', function() {
     var result = isEmailValid('abc');
-    expect(result).toBe(true);
+    expect(result).toBe(false);
   });
 
   it('should return true for you@example.com', function() {
-    isEmailValid('you@example.com');
-    expect(result).toBe(false);
-    expect(result).toBeFalsy();
+    var result =isEmailValid('you@example.com');
+    expect(result).toBe(true);
+    //expect(result).toBeFalsy();
   });
 
 });
   
 describe('verifyPassword', function() {
   beforeEach(function() {
-    var pass = "123";
+    var pass = "12345678";
   });
 
   it('should ...', function() {
-    verifyPassword(pass);
+    var result =verifyPassword("");
+    expect(result.isValid).toBe(false);
   });
 
   it('should ...', function() {
-    verifyPassword(pass);
+    var result =verifyPassword("12345678");
+    expect(result.isValid).toBe(true);
+  });
+
+});
+
+describe('isWithdrawValid', function() {
+
+  // SET UP: do something before each 'it' test aka unit test
+  beforeEach(function() {
+    
+  });
+
+  // TEAR DOWN: do something after each
+  afterEach(function() {
+    
+  });
+
+  it('should return false when the withdraw amount is negative', function() {
+    var result = isWithdrawValid('-200','10000');
+    expect(result.isValid).toBe(false);
+  });
+  it('should return false when the withdraw amount has characters besides numbers', function() {
+    var result = isWithdrawValid('ab9?!c','10000');
+    expect(result.isValid).toBe(false);
+  });
+  it('should return false when the withdraw amount is more than the available balance', function() {
+    var result = isWithdrawValid('20000','10000');
+    expect(result.isValid).toBe(false);
+  });
+  it('should return false when the withdraw amount has more than 2 decimal places', function() {
+    var result = isWithdrawValid('9.333','10000');
+    expect(result.isValid).toBe(false);
+  });
+
+  it('should return true for 9252.25', function() {
+    var result =isWithdrawValid('9252.25','10000');
+    expect(result.isValid).toBe(true);
+    
   });
 
 });
